ec4b33ba05ea358a436006c5d9fc1bdd042944b9
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-09-05 12:11:02 -0700

    Create U1L04.py

---
+++ Unit1/U1L04.py
@@ -0,0 +1,10 @@
+my_name = input("What is your name? ")
+print(my_name)
+print(type(my_name))
+
+my_age = float(input("How old are you? "))
+next_year = my_age + 1
+print(my_age)
+print(type(my_age))
+print(next_year)
+print(my_name,"next year you will be",next_year,"years old.")
