794798b6e336632a4ebf8985f01d6b04359eb7c3
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-22 17:20:05 -0700

    Create Unit4_HW1.py

---
+++ Unit4Code/Unit4_HW1.py
@@ -0,0 +1,31 @@
+"""
+Header goes here
+"""
+def mult_table(m,n):
+    """
+    Description goes here
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+
+    """
+
+
+#Excercise 2
+    
+
+#Exercise 3
+
+
+
+####Test code here####
+
+#Exercise 1 tests
+
+
+#Exercise 2 tests
+
