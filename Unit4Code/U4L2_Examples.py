5f3c58c34d511758c469be5f5ce1eb4ba78414b1
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-22 21:09:47 -0700

    Create U4L2_Examples.py

---
+++ Unit4Code/U4L2_Examples.py
@@ -0,0 +1,19 @@
+'''Example 1 - write a function that takes two numbers and 
+returns the floor division (//) of them'''
+
+
+
+
+
+'''Example 2 - Write a program that will calculate and 
+print the perimeter of a rectangle where its length and
+width is given by the user.
+
+To compute the perimeter, write a function named 
+calculate_perimeter that takes two parameters, width and
+height. The parameter should be given a default value 
+of 10.
+
+If the user enters a length value of 0 or less, call 
+calculate_perimeter and use the default value. 
+Otherwise, use the length value given as the parameter value.'''
